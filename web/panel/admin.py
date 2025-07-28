cc1128896115e661e9952eb657e83068ad598677
verhovv <vladimirverhov@yandex.ru>
2025-07-28 17:10:51 +1000

    v1

--- web/panel/admin.py
+++ web/panel/admin.py
@@ -50,9 +50,6 @@ class UserReferralAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
-
     def has_change_permission(self, request, obj=None):
         return False
 
